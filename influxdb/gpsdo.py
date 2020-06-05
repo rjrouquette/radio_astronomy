d86da3be014f72c3b0fb37c5f073c957e3302d2f
Robert J. Rouquette <robert.rouquette@focusvq.com>
2020-06-05 10:48:27 -0500

    add gps fix information to gpsdo.py

--- influxdb/gpsdo.py
+++ influxdb/gpsdo.py
@@ -5,20 +5,28 @@ import requests
 import time
 import json
 
-host = '192.168.3.155'
+gpsdoHost = '192.168.3.155'
+gpsdoUrl = 'http://'+gpsdoHost+'/'
+influxUrl = 'http://192.168.3.200:8086/write?db=radio_astronomy'
 
 while True:
     try:
-        r = requests.get('http://'+host+'/', timeout=1)
+        r = requests.get(gpsdoUrl, timeout=1)
         lines = r.text.split('\n')
         locked = lines[0].split(': ')[1].strip() == 'yes'
         error = float(lines[1].split(': ')[1].strip().split()[0])
         rmse = float(lines[2].split(': ')[1].strip().split()[0])
+        body = 'gpsdo,host=%s locked=%d,pll_error=%.1f,pll_rmse=%.1f %d\n' % (gpsdoHost, locked, error, rmse, int(time.time() * 1000000000))
 
-        body = 'gpsdo,host=%s locked=%d,pll_error=%.1f,pll_rmse=%.1f %d' % (host, locked, error, rmse, int(time.time() * 1000000000))
-        requests.post('http://192.168.3.200:8086/write?db=radio_astronomy', data=body, timeout=1)
+        gpsFix = lines[4].strip().split(',')
+        fixType = int(gpsFix[6])
+        fixSats = int(gpsFix[7])
+        fixDop = float(gpsFix[8])
+
+        body += 'gpsdo,host=%s gps_fix=%d,gps_sats=%d,gps_dop=%.2f %d\n' % (gpsdoHost, fixType, fixSats, fixDop, int(time.time() * 1000000000))
+        requests.post(influxUrl, data=body, timeout=1)
     except:
-        print('failed to poll or record stats');
+        print('failed to poll or record stats')
 
     # 10 second update interval
     time.sleep(10)
