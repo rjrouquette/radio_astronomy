351e99b9488ba9e10e85ebdfa16f8c0805744500
Robert J. Rouquette <robert.rouquette@focusvq.com>
2020-06-11 21:36:37 -0500

    improve tracking accuracy

--- avr/gpsdo/gpsdo.c
+++ avr/gpsdo/gpsdo.c
@@ -271,14 +271,14 @@ inline void onRisingPPS() {
         if(deltaError > 0) {
             incFeedback(step);
         }
-        else if(deltaError == 0 && step > 1) {
+        else if(deltaError == 0 && step <= 1) {
             incFeedback(1);
         }
     } else {
         if(deltaError < 0) {
             decFeedback(step);
         }
-        else if(deltaError == 0 && step > 1) {
+        else if(deltaError == 0 && step <= 1) {
             decFeedback(1);
         }
     }
